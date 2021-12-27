cc4123553276f56b5d5be3640e2c01804f373e50
KriTiKall <dart2002mix@mail.ru>
2021-12-27 16:30:25 +0500

    SDA configure liquibase and delete java pac with deprecated codes

--- sda-db/db/changelog/init/sql/create-all-table.sql
+++ sda-db/db/changelog/init/sql/create-all-table.sql
@@ -2,24 +2,24 @@
 create schema if not exists model;
 
 create table if not exists model.schedules(
-	id serial,
+	id serial primary key,
 	name varchar(10),
 	date date,
 	hash int
 );
 
 create table if not exists model.disciplines(
-	id serial,
+	id serial primary key,
 	name text
 );
 
 create table if not exists model.teacher_posts(
-	id serial,
+	id serial primary key,
 	name text
 );
 
 create table if not exists model.teachers(
-	id serial,
+	id serial primary key,
 	post_id int,
 	name varchar(100)
 );
@@ -29,7 +29,7 @@ create type model.pair_pos as enum('1', '2', '3', '4', '5', '6', '7', '8');
 create type model.lesson_format as enum('left', 'right', 'full');
 
 create table if not exists model.lessons(
-	id serial,
+	id serial primary key,
 	schedule_id int not null,
 	discipline_id int not null,
 	teacher_id int not null,
@@ -40,7 +40,7 @@ create table if not exists model.lessons(
 );
 
 create table if not exists model.schedule_updates(
-	id serial,
+	id serial primary key,
 	schedule_id int not null,
     uploaded timestamp not null,
 	last_update timestamp
