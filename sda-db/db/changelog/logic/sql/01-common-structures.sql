9dff69b108fdb39170753ac7e9cc8c2bde9689dd
geniyc <dart2002mix@mail.ru>
2022-02-21 06:02:52 +0500

    SDA change the project tree

--- sda-db/db/changelog/logic/sql/01-common-structures.sql
+++ sda-db/db/changelog/logic/sql/01-common-structures.sql
@@ -16,23 +16,22 @@ comment on type model.schedule is 'Store object data from application server';
 
 create table if not exists model.schedules
 (
-    id   serial primary key,
-    name varchar(10) not null,
-    date date        not null,
-    hash int         not null
+    id      serial primary key,
+    name_id int  not null,
+    date    date not null,
+    hash    int  not null
 );
 
 comment on table model.schedules is 'Basic schedule info';
 comment on column model.schedules.id is 'ID';
-comment on column model.schedules.name is 'Group name';
+comment on column model.schedules.name_id is 'Id on group name table';
 comment on column model.schedules.date is 'Schedule date';
 comment on column model.schedules.hash is 'Group name';
 
-create type model.entity_lesson as
+create table if not exists model.group_name
 (
-    first_id  int,
-    second_id int,
-    is_single bool
+    id   serial primary key,
+    name varchar(10) not null
 );
 
 create table if not exists model.lessons_to_schedule
@@ -41,6 +40,21 @@ create table if not exists model.lessons_to_schedule
     position    int                 not null check ( value < 8 and value >= 0),
     hash        int                 not null,
     lesson_ids  model.entity_lesson not null
+);
+
+create type model.entity_lesson as
+(
+    first_id  int,
+    second_id int,
+    is_single bool
+);
+
+create table if not exists model.lessons
+(
+    id            serial primary key,
+    discipline_id int         not null,
+    teacher_id    int         not null,
+    auditorium    varchar(20) not null
 );
 
 create table if not exists model.disciplines
@@ -60,14 +74,6 @@ create table if not exists model.teachers
     id      serial primary key,
     post_id int,
     name    varchar(80)
-);
-
-create table if not exists model.lessons
-(
-    id            serial primary key,
-    discipline_id int         not null,
-    teacher_id    int         not null,
-    auditorium    varchar(20) not null
 );
 
 -- In development
@@ -91,7 +97,7 @@ declare
 begin
 
     select into schedule_id id, schedule_hash hash
-    from model.schedules
+    from model.schedules sch join model.group_name gn on gn.id = sch.name_id
     where date = pi_schedule.date
       and name = pi_schedule.name;
 
@@ -113,7 +119,8 @@ exception
                 exception_diag text;
             begin
                 get stacked diagnostics exception_diag = pg_exception_context;
-                po_result_msg := result_test || ', status: ' || SQLSTATE || ', ' || SQLERRM || ' CTX:' || exception_diag;
+                po_result_msg := result_test || ', status: ' || SQLSTATE || ', ' || SQLERRM || ' CTX:' ||
+                                 exception_diag;
                 return;
             end;
 end;
@@ -125,51 +132,58 @@ create function model.insert_schedule(pi_schedule model.schedule,
 as
 $$
 declare
-    les_pos   model.entity_lesson;
-    less      model.lessons_json;
-    first_id  int;
-    second_id int;
-    sch_id    int;
+    p_les_pos       model.entity_lesson;
+    p_less          model.lessons_json;
+    p_group_name_id int;
+    p_first_id      int;
+    p_second_id     int;
+    p_schedule_id   int;
 begin
 
-    less = pi_schedule.lessons;
+    p_less = pi_schedule.lessons;
+
+    select id into p_group_name_id from model.teacher_posts where name = pi_schedule.name;
+    if not FOUND then
+        insert into model.group_name(name) values (pi_schedule.name);
+        select currval(pg_get_serial_sequence('model.group_name', 'id')) into p_group_name_id;
+    end if;
 
     insert into model.schedules values (pi_schedule.name, pi_schedule.date, pi_schedule.hash);
-    select currval(pg_get_serial_sequence('model.schedules', 'id')) into sch_id;
+    select currval(pg_get_serial_sequence('model.schedules', 'id')) into p_schedule_id;
 
     for i in 0..7
         loop
-            if less[i] ->> 'type' = 'model.entity.PairLesson' then
+            if p_less[i] ->> 'type' = 'model.entity.PairLesson' then
 --              is single flag (false)
-                les_pos.is_single = 'false';
+                p_les_pos.is_single = 'false';
 
-                if less[i] #>> '{pair,first,type}' != 'Empty' then
-                    select model.insert_lesson(less[i], 'pair,first') into first_id;
-                    les_pos.first_id := first_id;
+                if p_less[i] #>> '{pair,first,type}' != 'Empty' then
+                    select model.insert_lesson(p_less[i], 'pair,first') into p_first_id;
+                    p_les_pos.first_id := p_first_id;
                 else
-                    les_pos.first_id := null;
+                    p_les_pos.first_id := null;
                 end if;
 
-                if less[i] #>> '{pair,second,type}' != 'Empty' then
-                    select model.insert_lesson(less[i], 'pair,second') into second_id;
-                    les_pos.second_id := second_id;
+                if p_less[i] #>> '{pair,second,type}' != 'Empty' then
+                    select model.insert_lesson(p_less[i], 'pair,second') into p_second_id;
+                    p_les_pos.second_id := p_second_id;
                 else
-                    les_pos.second_id := null;
+                    p_les_pos.second_id := null;
                 end if;
             else
 --               is single flag (true)
-                les_pos.is_single = 'true';
+                p_les_pos.is_single = 'true';
 
-                if less[i] #>> '{lesson,type}' != 'Empty' then
-                    select model.insert_lesson(less[i], 'lesson') into first_id;
-                    les_pos.first_id := first_id;
+                if p_less[i] #>> '{lesson,type}' != 'Empty' then
+                    select model.insert_lesson(p_less[i], 'lesson') into p_first_id;
+                    p_les_pos.first_id := p_first_id;
                 else
-                    les_pos.first_id := null;
+                    p_les_pos.first_id := null;
                 end if;
             end if;
 
             insert into model.lessons_to_schedule(schedule_id, position, hash, lesson_ids)
-            values (sch_id, i, pi_schedule.hash, les_pos);
+            values (p_schedule_id, i, pi_schedule.hash, p_les_pos);
         end loop;
 
     po_result_msg := 'insert_schedule: ok';
@@ -196,14 +210,13 @@ declare
     p_teacher_id      int;
     p_teacher_name    varchar(80);
     p_teacher_post_id int;
-    p_teacher_post text;
+    p_teacher_post    text;
     p_discipline_id   int;
     p_discipline_name text;
 begin
-
-    -- is exists teacher
+    -- if not exist create new
+    -- is exists teacher post
     p_teacher_post = pi_lesson #>> '{' || pi_path || ',teacherPost}';
-
     select id into p_teacher_post_id from model.teacher_posts where name = p_teacher_post;
     if not FOUND then
         insert into model.teacher_posts(name) values (p_teacher_name);
@@ -212,7 +225,6 @@ begin
 
     -- is exists teacher
     p_teacher_name = pi_lesson #>> '{' || pi_path || ',teacherName}';
-
     select id into p_teacher_id from model.teachers where name = p_teacher_name;
     if not FOUND then
         insert into model.teachers(post_id, name) values (p_teacher_post_id, p_teacher_name);
@@ -221,7 +233,6 @@ begin
 
     -- is exists discipline
     p_discipline_name = pi_lesson #>> '{' || pi_path || ',name}';
-
     select id into p_discipline_id from model.disciplines where name = p_discipline_id;
     if not FOUND then
         insert into model.disciplines values (p_discipline_name);
